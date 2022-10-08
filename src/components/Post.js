6d899e166ed75dd1bf439c0b4e6593ac0cd3476d
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-10-08 18:50:10 +0600

    Link users to post

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,8 +5,10 @@ const Post = ({ post }) => {
     const { id, title, body } = post;
     return (
         <div className="rounded border bg-slate-100 p-6 text-left shadow">
-            <h4 className="mb-2 text-xl font-semibold">{title}</h4>
-            <p>{body}</p>
+            <h4 className="mb-2 text-xl font-semibold">
+                {title.length > 30 ? title.slice(0, 30) + "..." : title}
+            </h4>
+            <p>{body.length > 100 ? body.slice(0, 100) + "..." : body}</p>
             <Link
                 className="mt-4 block rounded bg-slate-700 py-2 px-4 text-center text-white transition hover:bg-slate-800"
                 to={`/blog/post/${id}`}>
--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const PostDetails = () => {
     const postDetails = useLoaderData();
-    const { title, body } = postDetails;
+    const { id, title, body, userId } = postDetails;
     return (
         <div className="container">
             <div className="p-6 text-left">
-                <h4 className="mb-2 text-xl font-semibold">{title}</h4>
+                <p>Post ID: {id}</p>
+                <h4 className="mb-2 text-xl font-semibold capitalize">{title}</h4>
                 <p>{body}</p>
+
+                <div className="block">
+                    <Link
+                        className="mt-4 inline-block rounded bg-slate-700 py-2 px-4 text-center text-white transition hover:bg-slate-800"
+                        to={`/user/${userId}`}>
+                        Get the author
+                    </Link>
+                </div>
             </div>
         </div>
     );
